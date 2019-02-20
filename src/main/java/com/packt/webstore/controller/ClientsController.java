549222dae2d55849fd1775e050137a4caf37fd22
Marcin_Smola <marcin.smola7@gmail.com>
2019-02-20 16:24:10 +0100

    added tomcat plugin

--- src/main/java/com/packt/webstore/controller/ClientsController.java
+++ src/main/java/com/packt/webstore/controller/ClientsController.java
@@ -4,7 +4,9 @@ import com.packt.webstore.service.ClientsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class ClientsController {
@@ -19,5 +21,10 @@ public class ClientsController {
     public String list(Model model){
         model.addAttribute("customers",clientsService.getAllCustomers());
         return "customers";
+    }
+    @GetMapping("/xd")
+    @ResponseBody
+    public String siema(){
+        return "XDDD";
     }
 }
